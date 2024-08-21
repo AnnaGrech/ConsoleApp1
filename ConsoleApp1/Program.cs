d5e37c91f30169688258c4a3f390e109354e080c
Виктория Дятчина <agr22@mail.ru>
2024-08-21 11:03:08 +0300

    Comments deleted

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,16 +18,3 @@ else
 
 
 
-
-//string s = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
-//Regex regex = new Regex(@"туп(\w*)");
-//MatchCollection matches = regex.Matches(s);
-//if (matches.Count > 0)
-//{
-//    foreach (Match match in matches)
-//        Console.WriteLine(match.Value);
-//}
-//else
-//{
-//    Console.WriteLine("Совпадений не найдено");
-//}
